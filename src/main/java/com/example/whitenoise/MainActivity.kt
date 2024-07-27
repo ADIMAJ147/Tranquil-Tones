1e4969881744ba4937882d2717fb1ffb1e2671ea
ADIMAJ147 <140270153+ADIMAJ147@users.noreply.github.com>
2024-07-27 23:32:12 +0530

    Add files via upload

---
+++ src/main/java/com/example/whitenoise/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.example.whitenoise
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val button1 = findViewById<Button>(R.id.button)
+        val button2 = findViewById<Button>(R.id.button2)
+
+        button1.setOnClickListener {
+            val intent = Intent(this, SoundActivity_1::class.java)
+            startActivity(intent)
+        }
+
+        button2.setOnClickListener {
+            val intent = Intent(this, SoundActivity_2::class.java)
+            startActivity(intent)
+        }
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
